7ef66cfee8035e1281cb6659f32916e3bda39878
zerrocxste <49279293+zerrocxste@users.noreply.github.com>
2021-06-28 21:57:54 +0600

    Update dllmain.cpp

--- bfv-hack/src/dllmain.cpp
+++ bfv-hack/src/dllmain.cpp
@@ -52,22 +52,6 @@ public:
 	float x, y, z;
 };
 
-class Matrix4x4
-{
-public:
-	union
-	{
-		struct
-		{
-			float        _11, _12, _13, _14;
-			float        _21, _22, _23, _24;
-			float        _31, _32, _33, _34;
-			float        _41, _42, _43, _44;
-		};
-		float m[4][4];
-	};
-};
-
 void thread(void* arg)
 {
 	console::attach("debug");
